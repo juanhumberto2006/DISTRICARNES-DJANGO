f6c1ba63374f5d4509faedd7d62cc21e5a185a6b
Juan Humberto <juanhumbertovega600@gmail.com>
2025-08-21 11:11:36 -0500

    Creacion de proyecto y aplicacion con django de  DISTRICARNES-DJANGO

---
+++ PROJECT_DISTRICARNES/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PROJECT_DISTRICARNES project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJECT_DISTRICARNES.settings')
+
+application = get_wsgi_application()
